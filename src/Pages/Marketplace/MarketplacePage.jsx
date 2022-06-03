2cf6b67d7df6f45bfe01469236d0be539e3ea27e
FarHowl <kartashov104@gmail.com>
2022-06-03 23:58:07 +0300

    Update MarketplacePage.jsx

--- src/Pages/Marketplace/MarketplacePage.jsx
+++ src/Pages/Marketplace/MarketplacePage.jsx
@@ -4,7 +4,7 @@ import box from "../../Images/Boxes/luckyBox.png";
 const rarityColor = {
     guarantee: "border-gray-400 border-opacity-50",
     common: "border-blue-100 border-opacity-50",
-    rare: "text-orangeborder-green-300 border-opacity-50",
+    rare: "text-green-300 border-green-300 border-opacity-50",
     epic: "border-blue-400 border-opacity-50",
     mythical: "border-purple-400 border-opacity-50",
     legendary: "text-yellow-300 border-yellow-300 border-opacity-50",
@@ -13,8 +13,18 @@ const rarityColor = {
 
 export default function MarketplacePage() {
     return (
-        <div className="w-full flex justify-center items-center flex-col bg-dark-purple-100 bg-opacity-10 shadow-lg rounded-xl relative h-full">
-            <div className="w-full lg:w-[1000px] flex justify-center p-4 flex-wrap gap-4">
+        <div className="w-full flex items-center flex-col relative h-full">
+            <div className="w-full lg:w-[1000px] shadow-lg rounded-xl bg-opacity-10 bg-dark-purple-100 flex justify-between p-4 pb-2">
+                <div className="w-[300px] flex justify-start">
+                    <img src="" alt="" />
+                    <input className="placeholder:italic placeholder:text-slate-500 rounded-md bg-black focus:border-green-500 focus:ring-blue-400 focus:ring-2" placeholder="Search for rabbits..." type="text" name="search"/>
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div className="w-full lg:w-[1000px] shadow-lg rounded-xl bg-opacity-10 bg-dark-purple-100 flex justify-center p-4 flex-wrap gap-6">
+                <ItemTile />
                 <ItemTile />
                 <ItemTile />
                 <ItemTile />
@@ -30,18 +40,18 @@ export default function MarketplacePage() {
 
 function ItemTile() {
     return (
-        <div className={"w-[250px] h-[290px] px-4 items-center flex flex-col rounded-md bg-dark-purple-300 border-2 justify-center " + rarityColor["legendary"]}>
+        <div className={"w-[250px] h-[290px] px-4 items-center flex flex-col rounded-md hover:bg-dark-purple-300 hover:mt-[-10px] animated-200 cursor-pointer border-2 justify-center " + rarityColor["legendary"]}>
             <div className="text-white w-full text-center py-2 font-bold text-2xl">
                 <span>Rabbit</span>
             </div>
             <div className="flex w-[150px] h-[150px]">
                 <img className="h-full w-full object-cover" src={box} alt="" />
             </div>
-            <div className="w-full justify-between flex py-3 border-t-2 border-gray-600 items-center">
+            <div className="w-full justify-between flex py-2 border-t-2 border-b-2 border-gray-800 items-center">
                 <div className="">
                     <span>Legendary</span>
                 </div>
-                <div className="text-purple-500">
+                <div className="text-white text-xl">
                     <span>300$</span>
                 </div>
             </div>
