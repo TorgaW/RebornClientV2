542391748d81c6f92cd340649892fda9f18e5081
FarHowl <kartashov104@gmail.com>
2022-06-17 19:08:20 +0300

    Update DepositOption.jsx

--- src/Pages/UserProfileOptions/DepositOption.jsx
+++ src/Pages/UserProfileOptions/DepositOption.jsx
@@ -11,6 +11,7 @@ import { catch401, logout } from "../../Utils/NetworkUtil";
 import { BigNumber, ethers } from "ethers";
 import { Decimals, ERC20Abi, GAMEAddress, HOTAddress } from "../../Utils/BlockchainUtils";
 import { isTabletOrMobileBrowser } from "../../Utils/BrowserUtil";
+import { isStringEmptyOrSpaces } from "../../Utils/StringUtil";
 // import { deleteUserDataFromStorage } from "../../Utils/LocalStorageManager/LocalStorageManager";
 // import { getUserDataFromStorage } from "../Utils/LocalStorageManager/LocalStorageManager";
 
@@ -91,9 +92,9 @@ export default function DepositOption() {
         }
     }
 
-    useEffect(()=>{
-        if(isMobile) navigate('/notfound');
-    },[isMobile])
+    useEffect(() => {
+        if (isMobile) navigate("/notfound");
+    }, [isMobile]);
 
     return showProfile ? (
         <div className="w-full flex flex-col text-white gap-4">
@@ -114,33 +115,63 @@ export default function DepositOption() {
             <div className="w-full flex justify-center p-4">
                 <div className="w-full max-w-[800px] flex flex-col gap-6 p-8 bg-dark-purple-300 rounded-lg">
                     <span className="text-xl font-semibold">
-                        Connected wallet:{" "}
-                        <span className="p-2 bg-zinc-600 bg-opacity-40 rounded-md">{metamask.isConnected ? metamask.wallet : "No wallets connected"}</span>
+                        Connected wallet: <span className="p-2 bg-zinc-600 bg-opacity-40 rounded-md">{metamask.isConnected ? metamask.wallet : "No wallets connected"}</span>
                     </span>
                     <span className="text-xl font-semibold">
-                        Your deposit: <span className="p-2 bg-zinc-600 bg-opacity-40 rounded-md">{balance.userBalance}</span>
+                        Your balance: <span className="p-2 bg-zinc-600 bg-opacity-40 rounded-md">{balance.userBalance}</span>
                     </span>
-                    <span className="text-xl font-semibold">
-                        You can deposit more <span className="text-purple-500">GAME</span> tokens here:
-                    </span>
-                    <input
-                        onWheel={(e) => {
-                            e.target.blur();
-                        }}
-                        type="number"
-                        min={0}
-                        placeholder="0"
-                        id="deposit"
-                        className="w-full tracking-wider max-w-[300px] text-xl font-semibold p-2 bg-dark-purple-500 rounded-lg hover:outline-none hover:ring-2 hover:ring-purple-400 focus:ring-purple-600"
-                    />
-                    <button
-                        onClick={() => {
-                            deposit();
-                        }}
-                        className="w-full max-w-[200px] p-4 text-xl font-semibold bg-purple-900 bg-opacity-50 rounded-md hover:bg-opacity-80 animated-100"
-                    >
-                        Make a deposit!
-                    </button>
+                    <div className="flex flex-col gap-8 w-full items-center">
+                        <div className="group animated-100 border-2 border-dark-purple-200 border-opacity-90 bg-dark-purple-100 bg-opacity-90 p-4 rounded-lg flex flex-col gap-3 items-center max-w-[650px] w-full">
+                            <span className="text-xl font-semibold">
+                                Deposit <span className="text-purple-500">GAME</span> tokens:
+                            </span>
+                            <div className="flex flex-col gap-4 items-center">
+                                <input
+                                    onWheel={(e) => {
+                                        e.target.blur();
+                                    }}
+                                    type="number"
+                                    min={0}
+                                    placeholder="0"
+                                    id="deposit"
+                                    className="w-full tracking-wider max-w-[300px] text-xl font-semibold p-2 bg-dark-purple-500 rounded-lg hover:outline-none hover:ring-2 hover:ring-purple-400 focus:ring-purple-600"
+                                />
+                                <button
+                                    onClick={() => {
+                                        deposit();
+                                    }}
+                                    className="w-full p-4 text-xl font-semibold bg-purple-900 bg-opacity-50 rounded-md hover:bg-opacity-80 animated-100"
+                                >
+                                    Make a deposit!
+                                </button>
+                            </div>
+                        </div>
+                        <div className="group animated-100 border-2 border-dark-purple-200 border-opacity-90 bg-dark-purple-100 bg-opacity-90 rounded-lg p-4 flex flex-col gap-3 items-center max-w-[650px] w-full">
+                            <span className="text-xl font-semibold">
+                                Withdraw <span className="text-purple-500">GAME</span> tokens:
+                            </span>
+                            <div className="flex flex-col gap-4 items-center">
+                                <input
+                                    onWheel={(e) => {
+                                        e.target.blur();
+                                    }}
+                                    type="number"
+                                    min={0}
+                                    placeholder="0"
+                                    id="withdraw"
+                                    className="w-full tracking-wider max-w-[300px] text-xl font-semibold p-2 bg-dark-purple-500 rounded-lg hover:outline-none hover:ring-2 hover:ring-purple-400 focus:ring-purple-600"
+                                />
+                                <button
+                                    onClick={() => {
+                                        deposit();
+                                    }}
+                                    className="w-full p-4 text-xl font-semibold bg-purple-900 bg-opacity-50 rounded-md hover:bg-opacity-80 animated-100"
+                                >
+                                    Withdraw!
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
